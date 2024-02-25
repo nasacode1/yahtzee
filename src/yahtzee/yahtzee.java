edf2a0f0340e1164e448ad619beac0f8386e13f8
Maanasa Ganesh <maanasarare@gmail.com>
2024-02-25 23:31:44 +0530

    Merge branch 'main' of https://github.com/nasacode1/yahtzee

--- src/yahtzee/yahtzee.java
+++ src/yahtzee/yahtzee.java
@@ -1,23 +1,10 @@
 package yahtzee;
-import java.util.*;
+
 public class yahtzee {
-	private dice die;
-	
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		yahtzee game = new yahtzee();
-		game.startGame();
-	}
-	
-	public void startGame() {
-		int number = 5;
-		die = new dice();
-		for(int i=0; i<3;i++) {
-			die.roll(number);
-		}
-		
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
