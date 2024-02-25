782e72deca1017edfaa0e36e47ee470903f516e2
Maanasa Ganesh <maanasarare@gmail.com>
2024-02-25 20:53:41 +0530

    implemented rollDice of dice class

--- src/yahtzee/dice.java
+++ src/yahtzee/dice.java
@@ -1,5 +1,15 @@
 package yahtzee;
+import java.util.*;
 
 public class dice {
+	public int[] roll(int number){
+		Random random = new Random();
+	
+		int[] values = new int[number];
+		for(int i=0;i<number;i++) {
+			values[i] = random.nextInt(6) + 1;
+		}
+		return values;
+	}
 
 }
--- src/yahtzee/yahtzee.java
+++ src/yahtzee/yahtzee.java
@@ -1,10 +1,23 @@
 package yahtzee;
-
+import java.util.*;
 public class yahtzee {
+	private dice die;
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		yahtzee game = new yahtzee();
+		game.startGame();
+	}
+	
+	public void startGame() {
+		int number = 5;
+		die = new dice();
+		for(int i=0; i<3;i++) {
+			die.roll(number);
+		}
+		
+		
 	}
 
 }
