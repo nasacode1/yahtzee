e1cf137d4bb079e0663ba915993611621736ced6
Maanasa Ganesh <maanasarare@gmail.com>
2024-02-26 11:59:35 +0530

    wrote player class

--- src/yahtzee/player.java
+++ src/yahtzee/player.java
@@ -1,5 +1,98 @@
 package yahtzee;
-
+import java.util.*;
 public class player {
+	public int totalDices(int[] values) {
+		int total= 0;
+		for(int value: values) {
+			total += value;
+		}
+		return total;
+	}
+	
+	public boolean verifyCount(int[] values, int reqCount, int num) {
+		int countTrack = 0;
+		for(int value:values) {
+			if(value == num) {
+				countTrack += 1;
+			}
+		}
+		return (countTrack == reqCount) ? true: false;
+	}
+	
+	public boolean hasSequence(int[] values, int length) {
+		int pointer = 0;
+		int countTrack = 0;
+		Arrays.sort(values);
+		int prev = values[0];
+		while(pointer < 5) {
+			if(values[pointer]-values[prev] > 1) {
+				countTrack = 0;
+			}
+			countTrack += 1;
+			prev = pointer;
+			pointer += 1;
+		}
+		return (countTrack == length) ? true:false;
+		
+	}
+	
+	
+	public int calculateScore(int[] values, Scanner scanner) {
+		System.out.println("Enter which category to calculate score in:");
+		System.out.println("1. Aces\n2. Twos\n3. Threes\n4. Fours\n5. Fives\n6. Sixes\n7.3 of a kind\n8.4 of a kind\n9. Full house\n10. Small straight\n11.Large straight\n12. Yahtzee\n13. Chance");
+		int currScore = 0;
+		int category = scanner.nextInt();
+		if(category < 7) {
+			for(int value:values) {
+				if(value == category) {
+					currScore += value;
+				}
+			}
+		}
+		switch(category) {
+		case 7:
+			System.out.println("Enter which number comes 3 of a kind");
+			int n1 = scanner.nextInt();
+			if(verifyCount(values, 3, n1)) {
+				currScore = totalDices(values);
+			}
+			else {
+				System.out.println("You dont fit in category "+ category);
+			}
+		case 8:
+			System.out.println("Enter which number comes 4 of a kind");
+			int n2 = scanner.nextInt();
+			if(verifyCount(values, 4, n2)) {
+				currScore = totalDices(values);
+			}
+			else {
+				System.out.println("You dont fit in category "+ category);
+			}
+		case 9:
+			System.out.println("This is full-house, ie, both 3 of a number and 2 of another number");
+			System.out.println("Enter which is 3 of a kind");
+			int n3= scanner.nextInt();
+			System.out.println("Enter which is 2 of a kind");
+			int n4= scanner.nextInt();
+			if(verifyCount(values, 3, n3) && verifyCount(values, 2, n4)) {
+				currScore =25;
+			}
+		case 10:
+			System.out.println("This is small straight, ie, sequence of 4 continuous numbers");
+			currScore = (hasSequence(values, 4)) ? 30: 0;
 
+		case 11:
+			System.out.println("This is small straight, ie, sequence of 4 continuous numbers");
+			currScore = (hasSequence(values, 5)) ? 40: 0;
+		
+		case 12: 
+			System.out.println("This is yahtzee");
+			System.out.println("Enter which number occurs in a count of 5.");
+			int n8 = scanner.nextInt();
+			currScore = (verifyCount(values, 5, n8)) ? 50:0;
+				
+			
+	}
+		return currScore;
+}
 }
--- src/yahtzee/yahtzee.java
+++ src/yahtzee/yahtzee.java
@@ -1,10 +1,35 @@
 package yahtzee;
-
+import java.util.*;
 public class yahtzee {
+	private dice die;
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		yahtzee game = new yahtzee();
+		game.startGame(scanner);
 	}
-
+	
+	public void startGame(Scanner scanner) {
+		int number = 5;
+		die = new dice();
+		int[] finaldices = new int[number];
+		int pointer = 0;
+		for(int i=0; i<3;i++) {
+			int[] values = die.roll(number);
+			System.out.println("This is round "+ i+1);
+			for(int j=0; j<number;j++) {
+				System.out.println("dice " + j+1 + ": " + values[j]);
+			}
+			System.out.println("Enter dice number you want to retain any dices : enter 7 to stop.");
+			int u=scanner.nextInt();
+			while(u != 7) {
+				finaldices[pointer] = values[u-1];
+				pointer += 1;
+				number -= 1;
+				u = scanner.nextInt();
+			}
+		}
+		//calculateScore
+	}
 }
